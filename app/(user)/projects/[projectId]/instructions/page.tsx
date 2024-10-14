12ed45fdd3278420cbd3e957d5f7655c650cdbc9
omrawat23 <omrawat23@gmail.com>
2024-10-14 07:46:14 +0530

    instructions updated

--- app/(user)/projects/[projectId]/instructions/page.tsx
+++ app/(user)/projects/[projectId]/instructions/page.tsx
@@ -32,22 +32,32 @@ export default function Page({ params }: { params: { projectId: string } }) {
   
   const reactEmbedCode = `import React, { useEffect, useRef } from 'react'
 
+// Disable the ESLint rule for this entire block
+/* eslint-disable @typescript-eslint/no-namespace */
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'my-widget': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-        'project-id': string;
+      "my-widget": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        projectid: string;
       };
     }
   }
 }
+/* eslint-enable @typescript-eslint/no-namespace */
 
-export default function MyWidget() {
+interface FeedbacifyWidgetProps {
+  projectId: string;
+}
+
+export default function FeedbacifyWidget({ projectId }: FeedbacifyWidgetProps) {
   const widgetRef = useRef<HTMLElement>(null)
 
   useEffect(() => {
     const script = document.createElement('script')
-    script.src = '${widgetUrl}/widget.umd.js'
+    script.src = 'https://feedbacify-widget.vercel.app/widget.umd.js'
     script.async = true
     document.body.appendChild(script)
 
@@ -56,7 +66,14 @@ export default function MyWidget() {
     }
   }, [])
 
-  return <my-widget ref={widgetRef} project-id="${params.projectId}"></my-widget>
+  return (
+    <div style={{position: "fixed", bottom: "15px", right: "15px"}}>
+      <my-widget
+        ref={widgetRef}
+        projectid={projectId}
+      />
+    </div>
+  )
 }
 `
 
@@ -110,7 +127,7 @@ export default function MyWidget() {
             </li>
             <li className="ml-8">
               <code className="bg-gray-200 p-1 rounded">
-                {'<MyWidget />'}
+              {'<FeedbacifyWidget projectId={${params.projectId}}/>'}
               </code>
             </li>
           </ol>
